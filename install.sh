ea1e3fbf657ce26d95449bd57c88d01dad0c043d
SamuelNittala <nittalasamuel7@gmail.com>
2021-07-08 02:05:05 +0530

    Added loading bar for gags

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 pkgs=("curl" "code --classic" "docker" "tilix" "postgresql" 
 	  "pgadmin4" "minikube" "postman" "xbindkeys")
-
+length=${#pkgs[@]}
+echo $length
 #pgadmin
 curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-read
 #install
 for pkg in "${pkgs[@]}" 
 do
+	echo loading  
 	sudo snap install $pkg || yes | sudo apt install $pkg
 done
--- quake.sh
+++ quake.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-echo "<---setting up tilix quake in $HOME" 
+
+#quake 
+echo "Setting up tilix quake in $HOME" 
+
+echo -ne '>>>                       [20%]\r'
 cd $HOME
+sleep 1
+
+echo -ne '>>>>>>>                   [40%]\r'
 chmod u+w . 
+sleep 1
+
+echo -ne '>>>>>>>>>>>>>>            [60%]\r'
 touch .xbindkeysrc
+sleep 1
+
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
 echo '"tilix --quake"' >> .xbindkeysrc 
 echo "F8" >> .xbindkeysrc
+sleep 1
+
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 xbindkeys --poll-rc
-echo "finished tilix setup--->"
+echo -ne '\n'
+echo "Finished tilix setup"
