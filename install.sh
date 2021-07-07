6286bdd8d90fe69d05360272b28ea32f2d13794e
SamuelNittala <nittalasamuel7@gmail.com>
2021-07-08 00:12:33 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pkgs=("curl" "code --classic" "docker" "tilix" "postgresql" "pgadmin4" "minikube")
+
+#pgadmin
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+#install
+for pkg in "${pkgs[@]}" 
+do
+	snap install $pkg || apt install $pkg
+done
