2b651989b710b31a4224d7405a5b06eb91b65d38
SamuelNittala <nittalasamuel7@gmail.com>
2021-07-27 15:46:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 pkgs=("curl" "code --classic" "docker" "tilix" "postgresql" 
-	  "pgadmin4" "dconf-cli" "minikube" "postman" "xbindkeys")
-length=${#pkgs[@]}
-echo $length
-#pgadmin
-curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-#install
+	  "dconf-cli" "minikube" "postman" "xbindkeys")
+
 for pkg in "${pkgs[@]}" 
 do
 	echo loading  
